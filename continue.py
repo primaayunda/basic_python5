3bbeaa6e1d1962be00acedc56d2d88c10670d24a
primaayunda <primariliayunda@gmail.com>
2021-03-20 15:06:18 +0700

    Pertemuan Ketiga

---
+++ continue.py
@@ -0,0 +1,20 @@
+# for i in range (5):
+#     if i == 3:
+#         continue
+#     print (i)
+
+# jadi, ini nilai 3-nya diskip
+
+# for i in range (5):
+#     if i == 3:
+#         break
+#     print (i)
+
+i = 1
+while True:
+    if i == 11:
+        break
+    print (i)
+    i += 1
+
+# disini pake break karena kondisi while kalo true program akan jalan terus
---
+++ range.py
@@ -0,0 +1,13 @@
+# range (start, stop, step)
+
+# x = range (6)
+# for n in x:
+#     print (n)
+
+# x = range (3, 6)
+# for n in x:
+#     print(n)
+
+x = range (1, 5, 2)
+for n in x:
+    print (n)
